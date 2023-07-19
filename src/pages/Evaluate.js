999938ea649c007fc3ef461459e0c33369872bbf
Charlesczc <charlesaaa555@gmail.com>
2023-07-20 02:03:57 +0800

    graph size changes

--- src/pages/Evaluate.js
+++ src/pages/Evaluate.js
@@ -2,17 +2,23 @@ function Evaluate() {
   return (
     <div>
       {/* <h1>Evaluate</h1> */}
-      <ul className="rocs">
-        <li>
-          <img src={process.env.PUBLIC_URL + "/graphs/svm_roc.svg"}></img>
-        </li>
-        <li>
-          <img src={process.env.PUBLIC_URL + "/graphs/rfc_roc.svg"}></img>
-        </li>
-        <li>
-          <img src={process.env.PUBLIC_URL + "/graphs/nn_roc.svg"}></img>
-        </li>
-      </ul>
+      <div className="rocs">
+        <img
+          src={process.env.PUBLIC_URL + "/graphs/svm_roc.svg"}
+          width="500px"
+          height="500px"
+        ></img>
+        <img
+          src={process.env.PUBLIC_URL + "/graphs/rfc_roc.svg"}
+          width="500px"
+          height="500px"
+        ></img>
+        <img
+          src={process.env.PUBLIC_URL + "/graphs/nn_roc.svg"}
+          width="500px"
+          height="500px"
+        ></img>
+      </div>
       <div className="matrix">
         <img src={process.env.PUBLIC_URL + "/graphs/matrix.svg"}></img>
       </div>
